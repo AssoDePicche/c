f9449c87e2da76d4bd1d51cc3eeba24de1fb71a0
AssoDePicche <samuelprado730@gmail.com>
2023-04-06 08:46:52 -0300

    style: format code

--- src/Introduction/Pointers in C/solution.c
+++ src/Introduction/Pointers in C/solution.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void update(int *a, int *b) {
-    int a_temp = *a;
-    
-    *a = a_temp + *b;
-    
-    *b = abs(a_temp - *b);
+void update(int *a, int *b)
+{
+  int temp = *a;
+
+  *a = temp + *b;
+
+  *b = abs(temp - *b);
 }
 
-int main() {
-    int a, b;
-    
-    int *a_pointer = &a, *b_pointer = &b;
-    
+int main()
+{
+    int a, b, *a_pointer = &a, *b_pointer = &b;
+
     scanf("%d %d", &a, &b);
-  
+
     update(a_pointer, b_pointer);
-  
+
     printf("%d\n%d", a, b);
 
     return 0;
