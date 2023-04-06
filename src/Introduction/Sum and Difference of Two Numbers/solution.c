506dde3bac6c0ca0b58b879182908fe1d5437ac9
AssoDePicche <samuelprado730@gmail.com>
2023-04-06 08:47:49 -0300

    style: format code

--- src/Introduction/Sum and Difference of Two Numbers/solution.c
+++ src/Introduction/Sum and Difference of Two Numbers/solution.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 
-int sum(int x, int y) {
+int sum(int x, int y)
+{
     return x + y;
 }
 
-float sub(float x, float y) {
+float sub(float x, float y)
+{
     return x - y;
 }
 
-int main() {
-    int a, b;
-    
+int main()
+{
+  int a, b;
+
 	float c, d;
-    
-    scanf("%d %d", &a, &b);
-    
-    scanf("%f %f", &c, &d);
-    
-    printf("%d %d\n%.1f %.1f", sum(a, c), sum(b, d), sub(a, c), sub(b, d));
-    
-    return 0;
+
+  scanf("%d %d", &a, &b);
+
+  scanf("%f %f", &c, &d);
+
+  printf("%d %d\n%.1f %.1f", sum(a, c), sum(b, d), sub(a, c), sub(b, d));
+
+  return 0;
 }
