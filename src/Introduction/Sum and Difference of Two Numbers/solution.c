51b2540b30a00c56c75ed64571f4115ffb27211b
AssoDePicche <samuelprado730@gmail.com>
2023-04-24 21:32:06 -0300

    refactor: improve sum and difference of two numbers design

--- src/Introduction/Sum and Difference of Two Numbers/solution.c
+++ src/Introduction/Sum and Difference of Two Numbers/solution.c
@@ -1,26 +1,16 @@
 #include <stdio.h>
 
-int sum(int x, int y)
-{
-    return x + y;
-}
-
-float sub(float x, float y)
-{
-    return x - y;
-}
-
 int main()
 {
   int a, b;
 
-	float c, d;
+  float c, d;
 
   scanf("%d %d", &a, &b);
 
   scanf("%f %f", &c, &d);
 
-  printf("%d %d\n%.1f %.1f", sum(a, c), sum(b, d), sub(a, c), sub(b, d));
+  printf("%d %d\n%.1f %.1f", (a + c), (b + d), (a - c), (b - d));
 
   return 0;
 }
