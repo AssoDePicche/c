58952852bf6f6c6428672d693f18cd27aeb37614
AssoDePicche <samuelprado730@gmail.com>
2023-04-25 22:39:06 -0300

    fix: correct solution

--- src/Introduction/Sum and Difference of Two Numbers/solution.c
+++ src/Introduction/Sum and Difference of Two Numbers/solution.c
@@ -6,11 +6,9 @@ int main()
 
   float c, d;
 
-  scanf("%d %d", &a, &b);
+  scanf("%d %d %f %f", &a, &b, &c, &d);
 
-  scanf("%f %f", &c, &d);
-
-  printf("%d %d\n%.1f %.1f", (a + c), (b + d), (a - c), (b - d));
+  printf("%d %d\n%.1f %.1f", a + b, a - b, c + d, c - d);
 
   return 0;
 }
