492f057819dba53aa45ddbc8e5f4103a0a912475
AssoDePicche <samuelprado730@gmail.com>
2023-05-06 13:14:30 -0300

    refactor: improve code design

--- src/Conditionals and Loops/Conditional Statements in C/solution.c
+++ src/Conditionals and Loops/Conditional Statements in C/solution.c
@@ -2,19 +2,19 @@
 
 int main()
 {
-  const char *lookup_table[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+  const char *table[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
-  int number;
+  int number, in_table;
 
   scanf("%d", &number);
 
-  if (number > 9)
+  in_table = (number > 0) && (number < 10);
+
+  if (in_table) {
+    printf("%s", table[number - 1]);
+  } else if (number > 9)
   {
     printf("Greater than 9");
-  }
-  else
-  {
-    printf("%s", lookup_table[number - 1]);
   }
 
   return 0;
